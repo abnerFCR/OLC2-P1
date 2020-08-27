0fcaa26a81e432d977e6201246521c1cb8afe98b
Abner  Cardona <abner.cardona1997@gmail.com>
2020-08-27 01:39:08 -0600

    Primer Commit, primera forma de la interfaz

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import "codemirror/lib/codemirror";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/javascript-hint";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  title = 'interprete-web';
+  entrada = "5*(1+3+20) - ((1+2)*(3+4))";
+  salida = "";
+  options: any = {
+    lineNumbers: true,
+    theme:'dracula',
+    //theme :'mbo',
+    lineWrapping : true,
+    indentWithTabs: true,
+    mode: 'javascript',
+
+    styleActiveLine: true
+    //extraKeys: {"Ctrl-Space": "autocomplete"}
+  };
+
+  
+
+  
+
+  ejecutar(){
+    try {
+      //const value = Parser.parse(this.entrada);
+      //this.salida = value + '';  
+    } catch (error) {
+      //alert("Aun no valido errores");
+
+    }
+  }
+}
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CodemirrorModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
